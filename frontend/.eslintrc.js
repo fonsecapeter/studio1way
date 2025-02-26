a361bdf7d14dfe9b739633405aa7e353a07a0bd3
Peter Fonseca <peter.nfonseca@gmail.com>
2025-02-26 10:08:06 -0800

    Fix frontend paths

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,23 +3,23 @@ module.exports = {
     node: true,
     browser: true,
   },
-  extends: 'airbnb-typescript',
+  extends: "airbnb-typescript",
   parser: "@typescript-eslint/parser",
   parserOptions: {
     "project": ["/app/frontend/tsconfig.json"]
   },
   plugins: [
-    '@typescript-eslint',
-    'promise',
-    'import',
+    "@typescript-eslint",
+    "promise",
+    "import",
   ],
   rules: {
-    'comma-dangle': 'off',
-    'global-require': 'off',
-    'import/prefer-default-export': 'off',
-    'react/destructuring-assignmen': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/require-default-props': 'off',
-    'react/prop-types': 'off',
+    "comma-dangle": "off",
+    "global-require": "off",
+    "import/prefer-default-export": "off",
+    "react/destructuring-assignmen": "off",
+    "react/jsx-filename-extension": "off",
+    "react/require-default-props": "off",
+    "react/prop-types": "off",
   }
 };
