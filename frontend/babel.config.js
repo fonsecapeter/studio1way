4d70709b5f24c4c02d46280dda07a573eaf4c5c3
Peter Fonseca <peter.nfonseca@gmail.com>
2025-03-02 18:08:25 -0800

    Finish removing babel plugins

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -6,14 +6,7 @@ module.exports = function (api) {
     "@babel/preset-react",
     "@babel/typescript",
   ];
-  const plugins = [
-    "@babel/plugin-transform-react-constant-elements",
-    "@babel/plugin-transform-react-inline-elements",
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/proposal-class-properties",
-    "@babel/proposal-object-rest-spread",
-    "dynamic-import-webpack",
-  ];
+  const plugins = [];
 
   return {
     presets,
