61b2edfb1e3dbf5674088f6f5a71cddc0b2ebd9c
Peter Fonseca <peter.nfonseca@gmail.com>
2025-03-23 21:20:20 -0700

    New logo

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logoPath from "../assets/img/web/logo.svg";
+import logoPath from "../assets/img/web/logo/25.jpg";
 import AppRoutes from "../routes";
 import Nav from "./nav/index";
 
 export const App = () => (
   <div className="main-wrapper">
     <div className="left-column">
-      <img src={logoPath} className="logo" alt="P. Fonseca" />
+      <img src={logoPath} className="logo" alt="Studio 1Way" />
       <Nav />
     </div>
     <div className="right-column">
